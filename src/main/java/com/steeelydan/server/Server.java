e2116d35f92002927a1c1478ff3b968c1d8faa1c
Steeely Dan <steeelydan@posteo.de>
2021-08-18 19:18:05 +0200

    Initial class

--- src/main/java/com/steeelydan/server/Server.java
+++ src/main/java/com/steeelydan/server/Server.java
@@ -1,5 +1,10 @@
 package com.steeelydan.server;
 
+/**
+ * Driver class for the HTTP Server
+ */
 public class Server {
-
+    public static void main(String[] args) {
+        System.out.println("Server starting");
+    }
 }

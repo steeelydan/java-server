d4db3fc0c59ebae1976a9be5599fc59d9bde93d2
Steeely Dan <steeelydan@posteo.de>
2021-08-21 20:21:53 +0200

    Add test for bad method

--- src/test/java/com/steeelydan/http/HttpParserTest.java
+++ src/test/java/com/steeelydan/http/HttpParserTest.java
@@ -1,6 +1,7 @@
 package com.steeelydan.http;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -22,19 +23,43 @@ public class HttpParserTest {
 
     @Test
     void parseHttpRequest() {
-        HttpRequest request = httpParser.parseHttpRequest(generateValidTestCase());
+        HttpRequest request;
+		try {
+			request = httpParser.parseHttpRequest(generateValidGETTestCase());
+            assertEquals(request.getMethod(), HttpMethod.GET);
+		} catch (HttpParsingException e) {
+			fail();
+		}
 
-        System.out.println(HttpMethod.GET);
-        assertEquals(request.getMethod(), HttpMethod.GET);
     }
 
-    private InputStream generateValidTestCase() {
+    @Test
+    void parseHttpRequestBadMethod() {
+        try {
+			httpParser.parseHttpRequest(generateInvalidGETTestCase());
+			fail();
+		} catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+		}
+    }
+
+    private InputStream generateValidGETTestCase() {
         String rawData = "GET / HTTP/1.1\r\n" + "Host: localhost:8080\r\n"
                 + "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0\r\n"
                 + "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
                 + "Accept-Language: en-US,en;q=0.5\r\n" + "Accept-Encoding: gzip, deflate\r\n" + "DNT: 1\r\n"
                 + "Connection: keep-alive\r\n" + "Upgrade-Insecure-Requests: 1\r\n" + "Sec-Fetch-Dest: document\r\n"
                 + "Sec-Fetch-Mode: navigate\r\n" + "Sec-Fetch-Site: none\r\n" + "Sec-Fetch-User: ?1\r\n"
+                + "Cache-Control: max-age=0\r\n" + "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateInvalidGETTestCase() {
+        String rawData = "GeT / HTTP/1.1\r\n" + "Host: localhost:8080\r\n"
+                + "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0\r\n"
                 + "Cache-Control: max-age=0\r\n" + "\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));

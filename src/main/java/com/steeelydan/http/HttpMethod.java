950be08ccf69c0cd58a6071562921f8ea793026d
Steeely Dan <steeelydan@posteo.de>
2021-08-21 22:11:08 +0200

    HttpMethod gets max length

--- src/main/java/com/steeelydan/http/HttpMethod.java
+++ src/main/java/com/steeelydan/http/HttpMethod.java
@@ -2,5 +2,18 @@ package com.steeelydan.http;
 
 public enum HttpMethod {
     GET,
-    HEAD
+    HEAD;
+
+    public static final int MAX_LENGTH;
+
+    static {
+        int tempMaxLength = -1;
+        for (HttpMethod method : values()) {
+            if (method.name().length() > tempMaxLength) {
+                tempMaxLength = method.name().length();
+            }
+        }
+
+        MAX_LENGTH = tempMaxLength;
+    }
 }
